43bc375c0fa498ac33dcf5a3144444cbaf4ead54
CuzIm1Tigaaa <cuz.tigaaa@gmail.com>
2021-04-13 17:31:05 +0200

    Made Strings final

--- src/main/java/de/cuzim1tigaaa/spectate/files/Permissions.java
+++ src/main/java/de/cuzim1tigaaa/spectate/files/Permissions.java
@@ -3,19 +3,19 @@ package de.cuzim1tigaaa.spectate.files;
 public class Permissions {
 
 
-    public static String COMMAND_SPECTATE_GENERAL = "spectator.commands.spectate";
-    public static String COMMAND_SPECTATE_OTHERS = "spectator.commands.spectateothers";
-    public static String COMMAND_SPECTATE_HERE = "spectator.commands.spectatehere";
-    public static String COMMAND_UNSPECTATE = "spectator.commands.unspectate";
-    public static String COMMANDS_SPECTATE_CYCLE = "spectator.commands.cycle";
-    public static String COMMANDS_SPECTATE_CYCLEONLY = "spectator.commands.cycleonly";
-    public static String COMMANDS_SPECTATE_LIST = "spectator.commands.list";
-    public static String COMMANDS_SPECTATE_RELOAD = "spectator.commands.reload";
+    public static final String COMMAND_SPECTATE_GENERAL = "spectator.commands.spectate";
+    public static final String COMMAND_SPECTATE_OTHERS = "spectator.commands.spectateothers";
+    public static final String COMMAND_SPECTATE_HERE = "spectator.commands.spectatehere";
+    public static final String COMMAND_UNSPECTATE = "spectator.commands.unspectate";
+    public static final String COMMANDS_SPECTATE_CYCLE = "spectator.commands.cycle";
+    public static final String COMMANDS_SPECTATE_CYCLEONLY = "spectator.commands.cycleonly";
+    public static final String COMMANDS_SPECTATE_LIST = "spectator.commands.list";
+    public static final String COMMANDS_SPECTATE_RELOAD = "spectator.commands.reload";
 
-    public static String BYPASS_TABLIST = "spectator.bypass.tablist";
-    public static String BYPASS_SPECTATED = "spectator.bypass.spectated";
-    public static String BYPASS_SPECTATEALL = "spectator.bypass.spectateall";
-    public static String UTILS_MIRROR_INVENTORY = "spectator.utils.mirrorinventory";
-    public static String UTILS_HIDE_IN_TAB = "spectator.utils.hidetab";
+    public static final String BYPASS_TABLIST = "spectator.bypass.tablist";
+    public static final String BYPASS_SPECTATED = "spectator.bypass.spectated";
+    public static final String BYPASS_SPECTATEALL = "spectator.bypass.spectateall";
+    public static final String UTILS_MIRROR_INVENTORY = "spectator.utils.mirrorinventory";
+    public static final String UTILS_HIDE_IN_TAB = "spectator.utils.hidetab";
 
 }

ccaf0b4e09132391b3160c623129ffb094d6b3fa
CuzIm1Tigaaa <cuz.tigaaa@gmail.com>
2021-08-02 20:45:54 +0200

    Updated to 1.6.7
    - Added update-notification onJoin
    - Changed some cycle things (hopefully they'll work...)
    - Split config from messages, renamed instance to plugin

--- src/main/java/de/cuzim1tigaaa/spectator/listener/PacketListener.java
+++ src/main/java/de/cuzim1tigaaa/spectator/listener/PacketListener.java
@@ -1,10 +1,9 @@
 package de.cuzim1tigaaa.spectator.listener;
 
 import com.comphenix.protocol.*;
-import com.comphenix.protocol.events.PacketAdapter;
-import com.comphenix.protocol.events.PacketEvent;
+import com.comphenix.protocol.events.*;
 import com.comphenix.protocol.wrappers.EnumWrappers;
-import de.cuzim1tigaaa.spectator.Main;
+import de.cuzim1tigaaa.spectator.Spectator;
 import de.cuzim1tigaaa.spectator.files.*;
 import de.cuzim1tigaaa.spectator.player.Inventory;
 import org.bukkit.GameMode;
@@ -12,9 +11,9 @@ import org.bukkit.entity.*;
 
 public class PacketListener {
 
-    private final Main instance;
+    private final Spectator instance;
 
-    public PacketListener(Main plugin) {
+    public PacketListener(Spectator plugin) {
         this.instance = plugin;
         this.register();
     }
@@ -25,24 +24,25 @@ public class PacketListener {
             @Override
             public void onPacketReceiving(PacketEvent event) {
                 Player player = event.getPlayer();
+                PacketContainer packet = event.getPacket();
 
                 if(!player.getGameMode().equals(GameMode.SPECTATOR)) return;
                 if(!instance.getSpectators().contains(player)) return;
 
-                EnumWrappers.EntityUseAction action = event.getPacket().getEntityUseActions().read(0);
+                EnumWrappers.EntityUseAction action = packet.getEntityUseActions().read(0);
                 if(!action.equals(EnumWrappers.EntityUseAction.ATTACK)) return;
 
-                Entity entity = event.getPacket().getEntityModifier(player.getWorld()).read(0);
+                Entity entity = packet.getEntityModifier(player.getWorld()).read(0);
                 if(!entity.getType().equals(EntityType.PLAYER)) return;
 
                 Player target = (Player) entity;
                 if(target.hasPermission(Permissions.BYPASS_SPECTATED) || !player.hasPermission(Permissions.COMMAND_SPECTATE_OTHERS)) {
                     if(!player.hasPermission(Permissions.BYPASS_SPECTATEALL)) {
-                        player.sendMessage(Config.getMessage(Paths.MESSAGES_GENERAL_BYPASS, "TARGET", target.getName()));
+                        player.sendMessage(Messages.getMessage(Paths.MESSAGES_GENERAL_BYPASS, "TARGET", target.getName()));
                         event.setCancelled(true);
                     }
                 }else {
-                    if(player.hasPermission(Permissions.UTILS_MIRROR_INVENTORY) && Config.mirrorInventory) Inventory.getInventory(player, target);
+                    Inventory.getInventory(player, target);
                     instance.getRelation().put(player, target);
                 }
             }
--- src/main/java/de/cuzim1tigaaa/spectator/listener/PlayerListener.java
+++ src/main/java/de/cuzim1tigaaa/spectator/listener/PlayerListener.java
@@ -1,8 +1,9 @@
 package de.cuzim1tigaaa.spectator.listener;
 
-import de.cuzim1tigaaa.spectator.Main;
+import de.cuzim1tigaaa.spectator.Spectator;
 import de.cuzim1tigaaa.spectator.cycle.CycleHandler;
 import de.cuzim1tigaaa.spectator.files.*;
+import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.*;
@@ -15,63 +16,75 @@ import java.util.Map;
 
 public class PlayerListener implements Listener {
 
-    private final Main instance;
+    private final Spectator plugin;
 
-    public PlayerListener(Main plugin) {
-        this.instance = plugin;
-        plugin.getServer().getPluginManager().registerEvents(this, instance);
+    public PlayerListener(Spectator plugin) {
+        this.plugin = plugin;
+        plugin.getServer().getPluginManager().registerEvents(this, this.plugin);
     }
 
-    @EventHandler
+    @EventHandler @SuppressWarnings("unused")
     public void onJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        if(!player.hasPermission(Permissions.BYPASS_TABLIST) && Config.hideTab) for(Player hidden : instance.getMethods().getHidden()) player.hidePlayer(instance, hidden);
-        else for(Player hidden : instance.getMethods().getHidden()) player.showPlayer(instance, hidden);
+
+        if(player.hasPermission(Permissions.NOTIFY_UPDATE_ON_JOIN) && this.plugin.getConfiguration().getBoolean(Paths.CONFIG_NOTIFY_UPDATE)) {
+            if(this.plugin.getUpdateChecker().isAvailable()) {
+                player.sendMessage(ChatColor.RED + "Spectator " + ChatColor.DARK_GRAY + "|" + ChatColor.GOLD + ChatColor.BOLD + " An Update is available! "
+                        + ChatColor.DARK_GRAY + "[" + ChatColor.YELLOW + "v" + plugin.getUpdateChecker().getVersion() + ChatColor.DARK_GRAY + "]");
+                player.sendMessage(ChatColor.DARK_GRAY + ChatColor.BOLD.toString() + "» " + ChatColor.YELLOW + "https://www.spigotmc.org/resources/spectator.93051/");
+            }
+        }
+        if(plugin.getConfiguration().getBoolean(Paths.CONFIG_PAUSE_WHEN_NO_PLAYERS)) {
+            for(Player all : CycleHandler.getPausedCycles().keySet()) CycleHandler.restartCycle(player);
+        }
+
+        if(!player.hasPermission(Permissions.BYPASS_TABLIST) && this.plugin.getConfiguration().getBoolean(Paths.CONFIG_HIDE_PLAYERS_TAB)) for(Player hidden : plugin.getMethods().getHidden()) player.hidePlayer(plugin, hidden);
+        else for(Player hidden : plugin.getMethods().getHidden()) player.showPlayer(plugin, hidden);
     }
-    @EventHandler
+    @EventHandler @SuppressWarnings("unused")
     public void onQuit(PlayerQuitEvent event) {
         Player player = event.getPlayer();
-        if(instance.getSpectators().contains(player)) instance.getMethods().unSpectate(player, false);
-        for (Map.Entry<Player, Player> entry : instance.getRelation().entrySet()) {
+        if(plugin.getSpectators().contains(player)) plugin.getMethods().unSpectate(player, false);
+        for (Map.Entry<Player, Player> entry : plugin.getRelation().entrySet()) {
             if (entry.getValue().equals(player)) {
                 Player spectator = entry.getKey();
-                if (!CycleHandler.isPlayerCycling(spectator)) instance.dismountTarget(spectator);
+                if (!CycleHandler.isPlayerCycling(spectator)) this.plugin.getMethods().dismountTarget(player);
                 else CycleHandler.restartCycle(spectator);
             }
         }
     }
-    @EventHandler
-    public void onPluginDisable(PluginDisableEvent event) { if(event != null) instance.disable(); }
-    @EventHandler
+    @EventHandler @SuppressWarnings("unused")
+    public void onPluginDisable(PluginDisableEvent event) { if(event != null) plugin.disable(); }
+    @EventHandler @SuppressWarnings("unused")
     public void onDismount(PlayerToggleSneakEvent event) {
         Player player = event.getPlayer();
-        if(instance.getSpectators().contains(player)) {
+        if(plugin.getSpectators().contains(player)) {
             if(!CycleHandler.isPlayerCycling(player) || !player.hasPermission(Permissions.COMMANDS_SPECTATE_CYCLEONLY)) {
-                if(event.isSneaking()) instance.dismountTarget(player);
+                if(event.isSneaking()) this.plugin.getMethods().dismountTarget(player);
             }else {
                 if(event.isSneaking()) {
-                    player.sendMessage(Config.getMessage(Paths.MESSAGES_GENERAL_DISMOUNT));
+                    player.sendMessage(Messages.getMessage(Paths.MESSAGES_GENERAL_DISMOUNT));
                     event.setCancelled(true);
                 }
             }
         }
     }
-    @EventHandler
+    @EventHandler @SuppressWarnings("unused")
     public void onGameModeChange(PlayerGameModeChangeEvent event) {
         Player player = event.getPlayer();
-        if(instance.getSpectators().contains(player)) {
-            player.sendMessage(Config.getMessage(Paths.MESSAGES_GENERAL_GAMEMODE_CHANGE));
+        if(plugin.getSpectators().contains(player)) {
+            player.sendMessage(Messages.getMessage(Paths.MESSAGES_GENERAL_GAMEMODE_CHANGE));
             event.setCancelled(true);
         }
     }
 
-    @EventHandler (priority = EventPriority.HIGHEST)
+    @EventHandler (priority = EventPriority.HIGHEST) @SuppressWarnings("unused")
     public void onKick(PlayerKickEvent event) {
         Player player = event.getPlayer();
-        if(!Config.kickOnCycle && CycleHandler.isPlayerCycling(player)) event.setCancelled(true);
+        if(!this.plugin.getConfiguration().getBoolean(Paths.CONFIG_KICK_WHILE_CYCLING) && CycleHandler.isPlayerCycling(player)) event.setCancelled(true);
     }
-    @EventHandler
+    @EventHandler @SuppressWarnings("unused")
     public void onInventoryClick(InventoryClickEvent event) { if (event.getWhoClicked().getGameMode().equals(GameMode.SPECTATOR)) event.setCancelled(true); }
-    @EventHandler
+    @EventHandler @SuppressWarnings("unused")
     public void onInventoryDrag(InventoryDragEvent event) { if (event.getWhoClicked().getGameMode().equals(GameMode.SPECTATOR)) event.setCancelled(true); }
 }

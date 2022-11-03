9579661966c231f1a41ffc9896ee22d7671d554e
CuzIm1Tigaaa <cuz.tigaaa@gmail.com>
2022-11-03 18:05:58 +0100

    Updated to 1.8
    - moved all events for inventory management to separate class
    - commented events
    - cleaned up all methods
    - fixed errors with pausing and resuming of cycle
    - fixed problems with portals (teleporting and still be in targets view)

---
+++ src/main/java/de/cuzim1tigaaa/spectator/listener/ContainerListener.java
@@ -0,0 +1,101 @@
+package de.cuzim1tigaaa.spectator.listener;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
+import de.cuzim1tigaaa.spectator.Spectator;
+import de.cuzim1tigaaa.spectator.files.*;
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.*;
+import org.bukkit.inventory.Inventory;
+
+import java.util.*;
+
+public class ContainerListener implements Listener {
+
+	private final Spectator plugin;
+
+	private final ImmutableSet<InventoryType> container = Sets.immutableEnumSet(InventoryType.BARREL, InventoryType.BLAST_FURNACE,
+			InventoryType.BREWING, InventoryType.CHEST, InventoryType.DISPENSER, InventoryType.DROPPER, InventoryType.FURNACE,
+			InventoryType.HOPPER, InventoryType.SMOKER, InventoryType.SHULKER_BOX, InventoryType.LECTERN);
+
+	public ContainerListener(Spectator plugin) {
+		this.plugin = plugin;
+		this.plugin.getServer().getPluginManager().registerEvents(this, plugin);
+	}
+
+	/**
+	 * Just in case, cancel clicking in inventories when in spectator mode
+	 */
+	@EventHandler
+	public void onInventoryClick(InventoryClickEvent event) {
+		Player player = (Player) event.getWhoClicked();
+		if(player.getGameMode() != GameMode.SPECTATOR) return;
+		event.setCancelled(true);
+	}
+
+	/**
+	 * Just in case, cancel dragging items around inventories when in spectator mode
+	 */
+	@EventHandler
+	public void onInventoryDrag(InventoryDragEvent event) {
+		Player player = (Player) event.getWhoClicked();
+		if(player.getGameMode() != GameMode.SPECTATOR) return;
+		event.setCancelled(true);
+	}
+
+	/**
+	 * Open container inventories for spectating players too
+	 */
+	@EventHandler
+	public void onChestOpen(InventoryOpenEvent event) {
+		Player player = (Player) event.getPlayer();
+		if(!this.plugin.getRelation().containsValue(player)) return;
+
+		Set<Player> spectators = new HashSet<>(this.plugin.getRelation().keySet());
+		spectators.removeIf(p -> !Objects.equals(p.getSpectatorTarget(), player));
+
+		Inventory inventory = null, openedInventory = event.getInventory();
+
+		if(openedInventory.getType() == InventoryType.ENDER_CHEST) {
+			spectators.removeIf(p -> !p.hasPermission(Permissions.UTILS_OPEN_ENDERCHEST));
+			if(Config.getBoolean(Paths.CONFIG_INVENTORY_ENDERCHEST)) inventory = player.getEnderChest();
+		}
+		if(this.container.contains(openedInventory.getType())) {
+			spectators.removeIf(p -> !p.hasPermission(Permissions.UTILS_OPEN_CONTAINER));
+			if(Config.getBoolean(Paths.CONFIG_INVENTORY_CONTAINERS)) inventory = openedInventory;
+		}
+		if(inventory != null) {
+			for(Player spec : spectators)
+				spec.openInventory(inventory);
+		}
+	}
+
+	/**
+	 * Close container inventories for spectating players too
+	 */
+	@EventHandler
+	public void onChestClose(InventoryCloseEvent event) {
+		Player player = (Player) event.getPlayer();
+		if(!this.plugin.getRelation().containsValue(player)) return;
+
+		Inventory openedInventory = event.getInventory();
+
+		if(openedInventory.getType() == InventoryType.ENDER_CHEST) {
+			if(!Config.getBoolean(Paths.CONFIG_INVENTORY_ENDERCHEST))
+				return;
+		}
+		if(this.container.contains(openedInventory.getType())) {
+			if(!Config.getBoolean(Paths.CONFIG_INVENTORY_CONTAINERS))
+				return;
+		}
+
+		Set<Player> spectators = new HashSet<>(this.plugin.getRelation().keySet());
+		spectators.removeIf(p -> !Objects.equals(p.getSpectatorTarget(), player));
+
+		for(Player spec : spectators)
+			spec.closeInventory();
+	}
+}
--- src/main/java/de/cuzim1tigaaa/spectator/listener/PlayerListener.java
+++ src/main/java/de/cuzim1tigaaa/spectator/listener/PlayerListener.java
@@ -2,186 +2,212 @@ package de.cuzim1tigaaa.spectator.listener;
 
 import com.destroystokyo.paper.event.player.PlayerAdvancementCriterionGrantEvent;
 import de.cuzim1tigaaa.spectator.Spectator;
-import de.cuzim1tigaaa.spectator.cycle.CycleHandler;
 import de.cuzim1tigaaa.spectator.files.*;
+import de.cuzim1tigaaa.spectator.player.Inventory;
 import de.cuzim1tigaaa.spectator.player.SpectateManager;
-import org.bukkit.*;
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.*;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.inventory.*;
 import org.bukkit.event.player.*;
-import org.bukkit.inventory.Inventory;
 
-import java.util.*;
+import java.util.Map;
+import java.util.UUID;
 
 public class PlayerListener implements Listener {
 
-    private final Spectator plugin;
-    private final SpectateManager manager;
+	private final Spectator plugin;
+	private final SpectateManager manager;
 
-    public PlayerListener(Spectator plugin) {
-        this.plugin = plugin;
-        this.plugin.getServer().getPluginManager().registerEvents(this, plugin);
-        this.manager = this.plugin.getSpectateManager();
-    }
+	public PlayerListener(Spectator plugin) {
+		this.plugin = plugin;
+		this.plugin.getServer().getPluginManager().registerEvents(this, plugin);
+		this.manager = this.plugin.getSpectateManager();
+	}
 
-    @EventHandler
-    public void onJoin(PlayerJoinEvent event) {
-        Player player = event.getPlayer();
+	/**
+	 * Notify admins, if the plugin has a newer version
+	 * Restart a paused cycle, when no players were online before
+	 * Hide spectating players in the tabList if enabled / set permissions allow it
+	 */
+	@EventHandler
+	public void playerJoin(PlayerJoinEvent event) {
+		Player player = event.getPlayer();
 
-        if(player.hasPermission(Permissions.NOTIFY_UPDATE_ON_JOIN) && Config.getBoolean(Paths.CONFIG_NOTIFY_UPDATE)) {
-            if(this.plugin.getUpdateChecker().isAvailable()) {
-                player.sendMessage(ChatColor.RED + "Spectator " + ChatColor.DARK_GRAY + "|" + ChatColor.GOLD + ChatColor.BOLD + " An Update is available! "
-                        + ChatColor.DARK_GRAY + "[" + ChatColor.YELLOW + "v" + this.plugin.getUpdateChecker().getVersion().replace("v", "") + ChatColor.DARK_GRAY + "]");
-                player.sendMessage(ChatColor.DARK_GRAY + ChatColor.BOLD.toString() + "» " + ChatColor.YELLOW + "https://www.spigotmc.org/resources/spectator.93051/");
-            }
-        }
-        if(Config.getBoolean(Paths.CONFIG_CYCLE_PAUSE_NO_PLAYERS))
-            for(Player paused : CycleHandler.getPausedCycles().keySet()) CycleHandler.restartCycle(paused);
+		if(player.hasPermission(Permissions.NOTIFY_UPDATE_ON_JOIN) && Config.getBoolean(Paths.CONFIG_NOTIFY_UPDATE)) {
+			if(this.plugin.getUpdateChecker().isUpdate())
+				this.sendUpdateNotification(player);
+		}
+		if(Config.getBoolean(Paths.CONFIG_CYCLE_PAUSE_NO_PLAYERS)) {
+			for(UUID paused : plugin.getCycleHandler().getPaused().keySet()) {
+				Player pausedPlayer = Bukkit.getPlayer(paused);
+				if(pausedPlayer == null || !pausedPlayer.isOnline()) {
+					this.plugin.getCycleHandler().getPaused().remove(paused);
+					continue;
+				}
+				this.plugin.getCycleHandler().restartCycle(pausedPlayer);
+			}
+		}
+		boolean hideSpectators = Config.getBoolean(Paths.CONFIG_HIDE_PLAYERS_TAB) &&
+				!player.hasPermission(Permissions.BYPASS_TABLIST);
 
-        boolean hide = !player.hasPermission(Permissions.BYPASS_TABLIST) && Config.getBoolean(Paths.CONFIG_HIDE_PLAYERS_TAB);
-        for(Player hidden : this.manager.getHidden()) {
-            if(hide) player.hidePlayer(this.plugin, hidden);
-            else player.showPlayer(this.plugin, hidden);
-        }
-    }
-    @EventHandler
-    public void onQuit(PlayerQuitEvent event) {
-        Player player = event.getPlayer();
-        if(this.plugin.getSpectators().contains(player)) this.manager.unSpectate(player, false);
-        for (Map.Entry<Player, Player> entry : this.plugin.getRelation().entrySet()) {
-            if (entry.getValue().equals(player)) {
-                Player spectator = entry.getKey();
-                if (!CycleHandler.isPlayerCycling(spectator)) this.manager.dismountTarget(spectator);
-                else {
-                    int onlineNonSpec = (Bukkit.getOnlinePlayers().size() - 1) - this.plugin.getSpectators().size();
-                    if(onlineNonSpec <= 0) {
-                        if(Config.getBoolean(Paths.CONFIG_CYCLE_PAUSE_NO_PLAYERS)) CycleHandler.pauseCycle(spectator);
-                        else CycleHandler.stopCycle(spectator);
-                    }else {
-                        spectator.setSpectatorTarget(null);
-                        CycleHandler.next(spectator);
-                    }
-                }
-            }
-        }
-    }
-    @EventHandler
-    public void onDismount(PlayerToggleSneakEvent event) {
-        Player player = event.getPlayer();
-        if(this.plugin.getSpectators().contains(player)) {
-            if(!CycleHandler.isPlayerCycling(player) || !player.hasPermission(Permissions.COMMANDS_SPECTATE_CYCLEONLY)) {
-                if(event.isSneaking()) this.manager.dismountTarget(player);
-            }else {
-                if(event.isSneaking() && this.plugin.getRelation().getOrDefault(player, null) != null) {
-                    player.sendMessage(Messages.getMessage(Paths.MESSAGES_GENERAL_DISMOUNT));
-                    event.setCancelled(true);
-                }
-            }
-        }
-    }
-    @EventHandler
-    public void onGameModeChange(PlayerGameModeChangeEvent event) {
-        Player player = event.getPlayer();
-        if(this.plugin.getSpectators().contains(player)) {
-            player.sendMessage(Messages.getMessage(Paths.MESSAGES_GENERAL_GAMEMODE_CHANGE));
-            event.setCancelled(true);
-        }
-    }
+		for(Player hidden : this.manager.getHidden()) {
+			if(hideSpectators) player.hidePlayer(this.plugin, hidden);
+			else player.showPlayer(this.plugin, hidden);
+		}
+	}
 
-    @EventHandler
-    public void onAdvancementCriteriaGrant(PlayerAdvancementCriterionGrantEvent event) {
-        Player player = event.getPlayer();
-        if(!plugin.getSpectators().contains(player)) return;
-        event.setCancelled(true);
-    }
+	/**
+	 * Pause a running cycle, when no more players are online
+	 */
+	@EventHandler
+	public void playerQuit(PlayerQuitEvent event) {
+		Player player = event.getPlayer();
+		if(this.plugin.getSpectators().contains(player))
+			this.manager.unSpectate(player, false);
 
-    @EventHandler(priority = EventPriority.HIGHEST) 
-    public void onKick(PlayerKickEvent event) {
-        Player player = event.getPlayer();
-        if(!Config.getBoolean(Paths.CONFIG_KICK_WHILE_CYCLING) && CycleHandler.isPlayerCycling(player)) {
-            event.setCancelled(true);
-        }
-    }
+		for(Map.Entry<Player, Player> entry : this.plugin.getRelation().entrySet()) {
+			if(entry.getValue().equals(player)) {
+				Player spectator = entry.getKey();
+				Bukkit.getScheduler().runTaskLater(plugin, () -> this.manager.dismountTarget(spectator), 5L);
+				if(plugin.getCycleHandler().isPlayerCycling(spectator)) {
+					if((Bukkit.getOnlinePlayers().size() - this.plugin.getSpectators().size() - 1) <= 0)
+						if(Config.getBoolean(Paths.CONFIG_CYCLE_PAUSE_NO_PLAYERS))
+							plugin.getCycleHandler().pauseRunningCycle(spectator);
+						else this.plugin.getCycleHandler().stopRunningCycle(spectator, true);
+					return;
+				}
+			}
+		}
+	}
 
-    @EventHandler
-    public void onDeath(PlayerDeathEvent event) {
-        Player player = event.getEntity();
-        if(this.plugin.getSpectators().contains(player)) this.plugin.getSpectateManager().unSpectate(player, false);
-        for (Map.Entry<Player, Player> entry : this.plugin.getRelation().entrySet()) {
-            if (entry.getValue().equals(player)) {
-                Player spectator = entry.getKey();
-                if (!CycleHandler.isPlayerCycling(spectator)) this.manager.dismountTarget(spectator);
-                else {
-                    spectator.setSpectatorTarget(null);
-                    CycleHandler.next(spectator);
-                }
-            }
-        }
-    }
+	/**
+	 * Normally a player can "leave" a spectator target with sneaking
+	 * This is disabled during spectate cycle (or if the player has the "CycleOnly" permission)
+	 */
+	@EventHandler
+	public void dismountTarget(PlayerToggleSneakEvent event) {
+		Player player = event.getPlayer();
+		if(this.plugin.getSpectators().contains(player)) {
+			if(plugin.getCycleHandler().isPlayerCycling(player) || player.hasPermission(Permissions.COMMANDS_SPECTATE_CYCLEONLY)) {
+				if(plugin.getRelation().getOrDefault(player, null) == null) return;
+				player.sendMessage(Messages.getMessage(Paths.MESSAGES_GENERAL_DISMOUNT));
+				event.setCancelled(true);
+				return;
+			}
+			this.manager.dismountTarget(player);
+		}
+	}
 
-    @EventHandler
-    public void onInventoryClick(InventoryClickEvent event) {
-        if (event.getWhoClicked().getGameMode().equals(GameMode.SPECTATOR)) event.setCancelled(true);
-    }
-    @EventHandler
-    public void onInventoryDrag(InventoryDragEvent event) { if (event.getWhoClicked().getGameMode().equals(GameMode.SPECTATOR)) event.setCancelled(true); }
+	/**
+	 * Disable changing gameMode while in plugins spectator mode
+	 */
+	@EventHandler
+	public void spectatorChangesGameMode(PlayerGameModeChangeEvent event) {
+		Player player = event.getPlayer();
+		if(this.plugin.getSpectators().contains(player)) {
+			player.sendMessage(Messages.getMessage(Paths.MESSAGES_GENERAL_GAMEMODE_CHANGE));
+			event.setCancelled(true);
+		}
+	}
 
-    @EventHandler
-    public void onChestOpen(InventoryOpenEvent event) {
-        Player player = (Player) event.getPlayer();
-        if(!this.plugin.getRelation().containsValue(player)) return;
+	/**
+	 * PaperMC only!
+	 * Used to cancel advancement progress while spectating
+	 */
+	@EventHandler
+	public void advancementCriteriaGrant(PlayerAdvancementCriterionGrantEvent event) {
+		Player player = event.getPlayer();
+		if(!plugin.getSpectators().contains(player)) return;
+		event.setCancelled(true);
+	}
 
-        Set<Player> spectators = new HashSet<>(this.plugin.getSpectators());
-        spectators.removeIf(p -> !this.plugin.getRelation().containsKey(p) || !this.plugin.getRelation().get(p).equals(player));
+	/**
+	 * When a player teleports through a portal, the spectator does not seem to be teleported with the player
+	 * This handles the change of a world, which will happen, when teleported by a portal
+	 */
+	@EventHandler
+	public void targetSwitchingWorld(PlayerChangedWorldEvent event) {
+		Player player = event.getPlayer();
+		if(plugin.getSpectators().contains(player)) return;
+		if(!plugin.getRelation().containsValue(player)) return;
 
-        Inventory inventory = null;
-        switch(event.getInventory().getType()) {
-            case BARREL, BLAST_FURNACE, BREWING, CHEST, DISPENSER, DROPPER, FURNACE, HOPPER, SMOKER, SHULKER_BOX, LECTERN -> {
-                spectators.removeIf(p -> !p.hasPermission(Permissions.UTILS_OPEN_CONTAINER));
-                if(Config.getBoolean(Paths.CONFIG_INVENTORY_CONTAINERS)) inventory = event.getInventory();
-            }
-            case ENDER_CHEST -> {
-                spectators.removeIf(p -> !p.hasPermission(Permissions.UTILS_OPEN_ENDERCHEST));
-                if(Config.getBoolean(Paths.CONFIG_INVENTORY_ENDERCHEST)) inventory = player.getEnderChest();
-            }
-        }
-        if(inventory != null) for(Player spec : spectators) spec.openInventory(inventory);
-    }
-    @EventHandler
-    public void onChestClose(InventoryCloseEvent event) {
-        Player player = (Player) event.getPlayer();
-        if(!this.plugin.getRelation().containsValue(player)) return;
+		for(Player spectator : plugin.getRelation().keySet()) {
+			if(plugin.getRelation().get(spectator).equals(player)) {
+				manager.dismountTarget(spectator);
+				spectator.teleport(player, PlayerTeleportEvent.TeleportCause.PLUGIN);
+				plugin.getRelation().put(spectator, player);
+			}
+		}
+	}
 
-        switch(event.getInventory().getType()) {
-            case WORKBENCH, STONECUTTER, CARTOGRAPHY, GRINDSTONE, ENCHANTING, COMPOSTER, SMITHING, CREATIVE, BEACON, ANVIL, LOOM, CRAFTING, MERCHANT, PLAYER -> { return; }
-            case ENDER_CHEST -> { if(!Config.getBoolean(Paths.CONFIG_INVENTORY_ENDERCHEST)) return; }
-            case BARREL, BLAST_FURNACE, BREWING, CHEST, DISPENSER, DROPPER, FURNACE, HOPPER, SMOKER, SHULKER_BOX, LECTERN -> { if(!Config.getBoolean(Paths.CONFIG_INVENTORY_CONTAINERS)) return; }
-        }
+	/**
+	 * Handles entering of a targets view (left click on player)
+	 * if the player has the bypass permission and the spectator don't have the equivalent bypass permission
+	 * the spectator cannot join the players view
+	 */
+	@EventHandler
+	public void spectatorEnterTarget(PlayerTeleportEvent event) {
+		if(event.getCause() != PlayerTeleportEvent.TeleportCause.SPECTATE) return;
 
-        Set<Player> spectators = new HashSet<>(this.plugin.getSpectators());
-        spectators.removeIf(p -> p.getSpectatorTarget() == null || !(p.getSpectatorTarget() instanceof Player)
-                || !p.getSpectatorTarget().getUniqueId().equals(player.getUniqueId()));
+		Player player = event.getPlayer();
+		if(!plugin.getSpectators().contains(player)) return;
 
-        for(Player spec : spectators) spec.closeInventory();
-    }
+		if(player.getSpectatorTarget() == null || !(player.getSpectatorTarget() instanceof Player target)) return;
+		if(!player.hasPermission(Permissions.COMMAND_SPECTATE_OTHERS)) {
+			event.setCancelled(true);
+			Bukkit.getScheduler().runTaskLater(plugin, () -> player.setSpectatorTarget(null), 5L);
+			return;
+		}
+		if(target.hasPermission(Permissions.BYPASS_SPECTATED) && !player.hasPermission(Permissions.BYPASS_SPECTATEALL)) {
+			event.setCancelled(true);
+			player.sendMessage(Messages.getMessage(Paths.MESSAGES_GENERAL_BYPASS_TELEPORT, "TARGET", target.getName()));
+			Bukkit.getScheduler().runTaskLater(plugin, () -> player.setSpectatorTarget(null), 5L);
+			return;
+		}
+		Inventory.getInventory(player, target);
+		plugin.getRelation().put(player, target);
+	}
 
-    @EventHandler
-    public void onTeleport(PlayerTeleportEvent event) {
-        if(event.getCause() != PlayerTeleportEvent.TeleportCause.SPECTATE) return;
-        Player player = event.getPlayer();
-        if(player.getSpectatorTarget() == null || !(player.getSpectatorTarget() instanceof Player target)) return;
+	/**
+	 * Cancel kickEvent, if player is in speccycle
+	 * mode AND the config option is enabled
+	 */
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void kickCyclingPlayer(PlayerKickEvent event) {
+		Player player = event.getPlayer();
+		if(!Config.getBoolean(Paths.CONFIG_KICK_WHILE_CYCLING) && plugin.getCycleHandler().isPlayerCycling(player)) {
+			event.setCancelled(true);
+		}
+	}
 
-        if(!player.hasPermission(Permissions.COMMAND_SPECTATE_OTHERS)) return;
-        if(target.hasPermission(Permissions.BYPASS_SPECTATED)) {
-            if(!player.hasPermission(Permissions.BYPASS_SPECTATEALL)) {
-                player.sendMessage(Messages.getMessage(Paths.MESSAGES_GENERAL_BYPASS_INVENTORY, "TARGET", target.getName()));
-                event.setCancelled(true);
-                return;
-            }
-        }
-        de.cuzim1tigaaa.spectator.player.Inventory.getInventory(player, target);
-        this.plugin.getRelation().put(player, target);
-    }
+	/**
+	 * Dismount from target, if the target died
+	 */
+	@EventHandler
+	public void targetDeathEvent(PlayerDeathEvent event) {
+		Player player = event.getEntity();
+		if(this.plugin.getSpectators().contains(player))
+			this.manager.unSpectate(player, false);
+
+		for(Map.Entry<Player, Player> entry : this.plugin.getRelation().entrySet()) {
+			if(entry.getValue().equals(player)) {
+				Player spectator = entry.getKey();
+				Bukkit.getScheduler().runTaskLater(plugin, () -> this.manager.dismountTarget(spectator), 5L);
+			}
+		}
+	}
+
+	/**
+	 * Send a message to the player to notify him that an update is available
+	 */
+	private void sendUpdateNotification(Player player) {
+		// &cSpectator &8| &6&lAn Update is available! &8[&ev1.2.3&8]
+		//  &8&l» &ehttps://www.spigotmc.org/resources/spectator.93051/
+		player.sendMessage(ChatColor.translateAlternateColorCodes('&', String.format("""
+			&cSpectator &8| &6&lAn Update is available! &8[&ev%s&8]
+			 &8&l» &ehttps://www.spigotmc.org/resources/spectator.93051/
+			""", this.plugin.getUpdateChecker().getVersion().replace("v", ""))));
+	}
 }

f1fb6f70b5d28d81d098003c7b2edfd07d742753
CuzIm1Tigaaa <cuz.tigaaa@gmail.com>
2023-12-12 22:19:24 +0100

    Rewrote all command classes for better code readability

--- src/main/java/de/cuzim1tigaaa/spectator/commands/Spectate.java
+++ src/main/java/de/cuzim1tigaaa/spectator/commands/Spectate.java
@@ -2,9 +2,8 @@ package de.cuzim1tigaaa.spectator.commands;
 
 import de.cuzim1tigaaa.spectator.Spectator;
 import de.cuzim1tigaaa.spectator.files.Paths;
-import de.cuzim1tigaaa.spectator.files.Permissions;
+import de.cuzim1tigaaa.spectator.spectate.SpectateUtils;
 import org.bukkit.Bukkit;
-import org.bukkit.GameMode;
 import org.bukkit.command.*;
 import org.bukkit.entity.Player;
 
@@ -12,116 +11,109 @@ import javax.annotation.Nonnull;
 import java.util.*;
 
 import static de.cuzim1tigaaa.spectator.files.Messages.getMessage;
+import static de.cuzim1tigaaa.spectator.files.Permissions.*;
 
 public class Spectate implements CommandExecutor, TabCompleter {
 
     private final Spectator plugin;
+    private final SpectateUtils spectateUtils;
 
     public Spectate(Spectator plugin) {
         Objects.requireNonNull(plugin.getCommand("spectate")).setExecutor(this);
         this.plugin = plugin;
+        this.spectateUtils = plugin.getSpectateUtils();
     }
 
     @Override
     public boolean onCommand(@Nonnull CommandSender sender, @Nonnull Command command, @Nonnull String label, @Nonnull String[] args) {
-        if(sender instanceof Player player) {
+        if(!(sender instanceof Player player)) {
             if(args.length == 0) {
-                spectateJoinSpectate(player, args);
+                sender.sendMessage(getMessage(Paths.MESSAGE_DEFAULT_SENDER));
                 return true;
             }
 
-            if(!player.hasPermission(Permissions.COMMAND_SPECTATE_OTHERS)) {
-                if(player.hasPermission(Permissions.COMMANDS_SPECTATE_CYCLEONLY)) {
-                    player.sendMessage(getMessage(Paths.MESSAGES_GENERAL_CYCLEONLY));
-                    return true;
-                }
-                player.sendMessage(getMessage(Paths.MESSAGE_DEFAULT_PERMISSION));
-                return true;
-            }
             Player target = Bukkit.getPlayer(args[0]);
-            if(target == null || !target.isOnline()) {
-                player.sendMessage(getMessage(Paths.MESSAGES_GENERAL_OFFLINEPLAYER, "TARGET", args[0]));
+
+            if(target == null) {
+                sender.sendMessage(getMessage(Paths.MESSAGES_GENERAL_OFFLINEPLAYER, "TARGET", args[0]));
                 return true;
             }
-            if(target.getUniqueId().equals(player.getUniqueId())) {
-                player.sendMessage(getMessage(Paths.MESSAGES_GENERAL_YOURSELF));
+
+            if(spectateUtils.isSpectator(target)) {
+                spectateUtils.Unspectate(target, true);
+                target.sendMessage(getMessage(Paths.MESSAGES_COMMANDS_SPECTATE_LEAVE_OWN));
+                sender.sendMessage(getMessage(Paths.MESSAGES_COMMANDS_SPECTATE_LEAVE_OTHER, "TARGET", target.getDisplayName()));
                 return true;
             }
-            if(this.plugin.getRelation().get(player) == target) {
-                player.sendMessage(getMessage(Paths.MESSAGES_GENERAL_SAMEPLAYER, "TARGET", target.getDisplayName()));
+
+            spectateUtils.Spectate(target, null);
+            target.sendMessage(getMessage(Paths.MESSAGES_COMMANDS_SPECTATE_JOIN_OWN));
+            sender.sendMessage(getMessage(Paths.MESSAGES_COMMANDS_SPECTATE_JOIN_OTHER, "TARGET", target.getDisplayName()));
+            return true;
+        }
+
+        if(!hasPermission(player, COMMAND_SPECTATE_GENERAL)) {
+            player.sendMessage(getMessage(Paths.MESSAGE_DEFAULT_PERMISSION));
+            return true;
+        }
+
+        if(args.length == 0 || !hasPermission(player, COMMAND_SPECTATE_OTHERS)) {
+            if(spectateUtils.isSpectator(player)) {
+                spectateUtils.Unspectate(player, true);
+                player.sendMessage(getMessage(Paths.MESSAGES_COMMANDS_SPECTATE_LEAVE_OWN));
                 return true;
             }
-            if(this.plugin.getRelation().get(target) == player || target.hasPermission(Permissions.BYPASS_SPECTATED)) {
-                if(!player.hasPermission(Permissions.BYPASS_SPECTATEALL)) {
-                    player.sendMessage(getMessage(Paths.MESSAGES_GENERAL_BYPASS_TELEPORT, "TARGET", target.getDisplayName()));
+
+            spectateUtils.Spectate(player, null);
+            player.sendMessage(getMessage(Paths.MESSAGES_COMMANDS_SPECTATE_JOIN_OWN));
+            return true;
+        }
+
+        Player target = Bukkit.getPlayer(args[0]);
+        if(target == null || !target.isOnline()) {
+            player.sendMessage(getMessage(Paths.MESSAGES_GENERAL_OFFLINEPLAYER, "TARGET", args[0]));
+            return true;
+        }
+
+        if(target.getUniqueId().equals(player.getUniqueId())) {
+            player.sendMessage(getMessage(Paths.MESSAGES_GENERAL_YOURSELF));
+            return true;
+        }
+
+        if(spectateUtils.isSpectating(player, target)) {
+            player.sendMessage(getMessage(Paths.MESSAGES_GENERAL_SAMEPLAYER, "TARGET", target.getDisplayName()));
+            return true;
+        }
+
+        if(spectateUtils.isSpectating(target, player) || hasPermission(target, BYPASS_SPECTATED)) {
+            if(!hasPermission(player, BYPASS_SPECTATEALL)) {
+                player.sendMessage(getMessage(Paths.MESSAGES_GENERAL_BYPASS_TELEPORT, "TARGET", target.getDisplayName()));
+                return true;
+            }
+        }
+
+        if(plugin.getMultiverseCore() != null) {
+            if(!player.getWorld().getUID().equals(target.getWorld().getUID())) {
+                String world = plugin.getMultiverseCore().getMVWorldManager().getMVWorld(target.getWorld()).getPermissibleName();
+                if(!player.hasPermission("multiverse.access." + world)) {
+                    player.sendMessage(getMessage(Paths.MESSAGES_COMMANDS_SPECTATE_MULTIVERSE, "TARGET", target.getDisplayName()));
                     return true;
                 }
             }
-
-            if(plugin.getMultiverse() != null) {
-                if(!Objects.equals(player.getWorld(), target.getWorld())) {
-                    String world = plugin.getMultiverse().getMVWorldManager().getMVWorld(target.getWorld()).getPermissibleName();
-                    if(!player.hasPermission("multiverse.access." + world)) {
-                        player.sendMessage(getMessage(Paths.MESSAGES_COMMANDS_SPECTATE_MULTIVERSE, "TARGET", target.getDisplayName()));
-                        return true;
-                    }
-                }
-            }
-
-            this.plugin.getSpectateManager().spectate(player, target);
-            this.plugin.getSpectateManager().notifyTarget(target, player, true);
-            player.sendMessage(getMessage(Paths.MESSAGES_COMMANDS_SPECTATE_PLAYER, "TARGET", target.getDisplayName()));
-            return true;
         }
 
-        if(args.length > 0) {
-            spectateFromConsole(sender, args);
-            return true;
-        }
-
-        sender.sendMessage(getMessage(Paths.MESSAGE_DEFAULT_SENDER));
+        spectateUtils.Spectate(player, target);
+        // TODO plugin.getSpectateManager().notifyTarget(target, player, true);
+        player.sendMessage(getMessage(Paths.MESSAGES_COMMANDS_SPECTATE_PLAYER, "TARGET", target.getDisplayName()));
         return true;
-    }
-
-    private void spectateJoinSpectate(Player player, String[] args) {
-        if(this.plugin.getSpectators().contains(player)) {
-            this.plugin.getSpectateManager().unSpectate(player, false);
-            player.sendMessage(getMessage(Paths.MESSAGES_COMMANDS_SPECTATE_LEAVE_OWN));
-            return;
-        }
-        if(!player.hasPermission(Permissions.COMMAND_SPECTATE_GENERAL)) {
-            if(player.hasPermission(Permissions.COMMANDS_SPECTATE_CYCLEONLY)) {
-                player.sendMessage(getMessage(Paths.MESSAGES_GENERAL_CYCLEONLY));
-                return;
-            }
-            player.sendMessage(getMessage(Paths.MESSAGE_DEFAULT_PERMISSION));
-            return;
-        }
-        this.plugin.getSpectateManager().spectate(player, null);
-        player.sendMessage(getMessage(Paths.MESSAGES_COMMANDS_SPECTATE_JOIN_OWN));
-    }
-
-    private void spectateFromConsole(CommandSender sender, String[] args) {
-        Player player = Bukkit.getPlayer(args[0]);
-        if(player == null) {
-            sender.sendMessage(getMessage(Paths.MESSAGES_GENERAL_OFFLINEPLAYER, "TARGET", args[0]));
-            return;
-        }
-        if(player.getGameMode().equals(GameMode.SPECTATOR)) {
-            this.plugin.getSpectateManager().unSpectate(player, false);
-            player.sendMessage(getMessage(Paths.MESSAGES_COMMANDS_SPECTATE_LEAVE_OWN));
-            sender.sendMessage(getMessage(Paths.MESSAGES_COMMANDS_SPECTATE_LEAVE_OTHER, "TARGET", player.getDisplayName()));
-            return;
-        }
-        this.plugin.getSpectateManager().spectate(player, null);
-        player.sendMessage(getMessage(Paths.MESSAGES_COMMANDS_SPECTATE_JOIN_OWN));
-        sender.sendMessage(getMessage(Paths.MESSAGES_COMMANDS_SPECTATE_JOIN_OTHER, "TARGET", player.getDisplayName()));
     }
 
     @Override
     public List<String> onTabComplete(@Nonnull CommandSender sender, @Nonnull Command command, @Nonnull String s, @Nonnull String[] args) {
-        if(args.length == 1)
-            return plugin.getOnlinePlayerNames();
+        if(args.length == 1) {
+            if(!(sender instanceof Player player) || hasPermission(player, COMMAND_SPECTATE_OTHERS))
+                return plugin.getOnlinePlayerNames();
+        }
         return Collections.emptyList();
     }
 }
--- src/main/java/de/cuzim1tigaaa/spectator/commands/UnSpectate.java
+++ src/main/java/de/cuzim1tigaaa/spectator/commands/UnSpectate.java
@@ -1,7 +1,9 @@
 package de.cuzim1tigaaa.spectator.commands;
 
 import de.cuzim1tigaaa.spectator.Spectator;
-import de.cuzim1tigaaa.spectator.files.*;
+import de.cuzim1tigaaa.spectator.files.Messages;
+import de.cuzim1tigaaa.spectator.files.Paths;
+import de.cuzim1tigaaa.spectator.spectate.SpectateUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.command.*;
 import org.bukkit.entity.Player;
@@ -9,46 +11,54 @@ import org.bukkit.entity.Player;
 import javax.annotation.Nonnull;
 import java.util.*;
 
+import static de.cuzim1tigaaa.spectator.files.Permissions.COMMAND_UNSPECTATE;
+import static de.cuzim1tigaaa.spectator.files.Permissions.hasPermission;
+
 public class UnSpectate implements CommandExecutor, TabCompleter {
 
     private final Spectator plugin;
+    private final SpectateUtils spectateUtils;
 
     public UnSpectate(Spectator plugin) {
         Objects.requireNonNull(plugin.getCommand("unspectate")).setExecutor(this);
         this.plugin = plugin;
+        this.spectateUtils = plugin.getSpectateUtils();
     }
 
     @Override
     public boolean onCommand(@Nonnull CommandSender sender, @Nonnull Command command, @Nonnull String label, @Nonnull String[] args) {
-        if(!sender.hasPermission(Permissions.COMMAND_UNSPECTATE)) {
+        if(!hasPermission(sender, COMMAND_UNSPECTATE)) {
             sender.sendMessage(Messages.getMessage(Paths.MESSAGE_DEFAULT_PERMISSION));
             return true;
         }
-        if(args.length < 1) {
-            if(this.plugin.getSpectators().size() <= 0) {
+
+        if(args.length == 0) {
+            if(spectateUtils.getSpectators().isEmpty()) {
                 sender.sendMessage(Messages.getMessage(Paths.MESSAGES_COMMANDS_LIST_NONE));
                 return true;
             }
-            this.plugin.getSpectateManager().restoreAll();
-            for(Player player : this.plugin.getSpectators())
-                if(!player.equals(sender))
-                    player.sendMessage(Messages.getMessage(Paths.MESSAGES_COMMANDS_SPECTATE_LEAVE_OWN));
+
+            for(Player spectator : spectateUtils.getSpectators()) {
+                if(spectator.equals(sender)) continue;
+                spectator.sendMessage(Messages.getMessage(Paths.MESSAGES_COMMANDS_SPECTATE_LEAVE_OWN));
+            }
+
+            // TODO plugin.getSpectateManager().restoreAll();
             sender.sendMessage(Messages.getMessage(Paths.MESSAGES_COMMANDS_UNSPECTATE_ALL));
             return true;
         }
         Player target = Bukkit.getPlayer(args[0]);
-        if(target == null) {
+        if(target == null || !target.isOnline()) {
             sender.sendMessage(Messages.getMessage(Paths.MESSAGES_GENERAL_OFFLINEPLAYER, "TARGET", args[0]));
             return true;
         }
-        if(!this.plugin.getSpectators().contains(target)) {
+
+        if(!spectateUtils.isSpectator(target)) {
             sender.sendMessage(Messages.getMessage(Paths.MESSAGES_GENERAL_NOTSPECTATING, "TARGET", target.getDisplayName()));
             return true;
         }
-        boolean useCurrentLocation = false;
-        if(args.length > 1) useCurrentLocation = Boolean.parseBoolean(args[1]);
 
-        this.plugin.getSpectateManager().unSpectate(target, useCurrentLocation);
+        spectateUtils.Unspectate(target, !(args.length > 1 && Boolean.parseBoolean(args[1])));
         target.sendMessage(Messages.getMessage(Paths.MESSAGES_COMMANDS_SPECTATE_LEAVE_OWN));
         sender.sendMessage(Messages.getMessage(Paths.MESSAGES_COMMANDS_UNSPECTATE_PLAYER, "TARGET", target.getDisplayName()));
         return true;
@@ -56,10 +66,10 @@ public class UnSpectate implements CommandExecutor, TabCompleter {
 
     @Override
     public List<String> onTabComplete(@Nonnull CommandSender sender, @Nonnull Command command, @Nonnull String s, @Nonnull String[] args) {
-        if(args.length == 1)
-            return plugin.getOnlinePlayerNames();
-        if(args.length == 2)
-            return List.of("true", "false");
-        return Collections.emptyList();
+        return switch(args.length) {
+            case 1 -> plugin.getOnlinePlayerNames();
+            case 2 -> List.of("true", "false");
+            default -> Collections.emptyList();
+        };
     }
 }

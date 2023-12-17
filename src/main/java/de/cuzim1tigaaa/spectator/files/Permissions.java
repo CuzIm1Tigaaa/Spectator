3935e023155711415a91f663556c85d6d51726cd
CuzIm1Tigaaa <cuz.tigaaa@gmail.com>
2023-12-17 22:49:45 +0100

    Fixed paths and permission names

--- src/main/java/de/cuzim1tigaaa/spectator/files/Permissions.java
+++ src/main/java/de/cuzim1tigaaa/spectator/files/Permissions.java
@@ -1,5 +1,7 @@
 package de.cuzim1tigaaa.spectator.files;
 
+import org.bukkit.command.CommandSender;
+
 public final class Permissions {
 
     public static final String COMMANDS_SPECTATE_RELOAD =    "spectator.commands.spectatereload";
@@ -10,7 +12,6 @@ public final class Permissions {
     public static final String COMMANDS_SPECTATE_LIST =      "spectator.commands.spectatelist";
     public static final String COMMANDS_SPECTATE_CYCLE =     "spectator.commands.spectatecycle.default";
     public static final String COMMANDS_CYCLE_STOP_OTHERS =  "spectator.commands.spectatecycle.stopOthers";
-    public static final String COMMANDS_SPECTATE_CYCLEONLY = "spectator.commands.spectatecycle.only";
 
     public static final String BYPASS_TABLIST =              "spectator.bypass.tablist";
     public static final String BYPASS_SPECTATED =            "spectator.bypass.spectated";
@@ -23,5 +24,17 @@ public final class Permissions {
     public static final String UTILS_MIRROR_EFFECTS =        "spectator.utils.mirroreffects";
     public static final String UTILS_HIDE_IN_TAB =           "spectator.utils.hidetab";
     public static final String NOTIFY_UPDATE_ON_JOIN =       "spectator.notify.update";
+
+    public static boolean hasPermission(CommandSender sender, String permission) {
+        return sender.hasPermission(permission);
+    }
+
+    public static boolean hasPermissions(CommandSender sender, String... permissions) {
+        for(String permission : permissions) {
+            if(!sender.hasPermission(permission))
+                return false;
+        }
+        return true;
+    }
 
 }

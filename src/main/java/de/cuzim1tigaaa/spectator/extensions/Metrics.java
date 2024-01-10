7c1ec345757073cec7d2cced46bfca28afd0a179
CuzIm1Tigaaa <cuz.tigaaa@gmail.com>
2024-01-10 17:08:08 +0100

    Moved files in new directory

---
+++ src/main/java/de/cuzim1tigaaa/spectator/extensions/Metrics.java
@@ -0,0 +1,12 @@
+package de.cuzim1tigaaa.spectator.extensions;
+
+import de.cuzim1tigaaa.spectator.Spectator;
+import org.bstats.charts.SingleLineChart;
+
+public class Metrics {
+
+	public Metrics(Spectator plugin) {
+		org.bstats.bukkit.Metrics metrics = new org.bstats.bukkit.Metrics(plugin, 12235);
+		metrics.addCustomChart(new SingleLineChart("spectating_players", () -> plugin.getSpectateUtils().getSpectators().size()));
+	}
+}
